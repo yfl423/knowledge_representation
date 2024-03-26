7a8d1495cea2e0dc69914a63280c09e09bb49414
fenglongyang <yfl423@tamu.edu>
2024-03-26 14:41:44 -0500

    Passed unit test

--- test/UnitTest.java
+++ test/UnitTest.java
@@ -7,31 +7,31 @@ import src.Core;
 public class UnitTest {
     @Test
     public void TestDEMO() {
-        Core core = new Core("../demo.in");
+        Core core = new Core("test/demo.in");
         core.run();
     }
 
     @Test
     public void TestTask1() {
-        Core core = new Core("/Users/fenglongyang/Downloads/knowledge_representation/task1.in");
+        Core core = new Core("test/task1.in");
         core.run();
     }
 
     @Test
     public void TestTask3() {
-        Core core = new Core("/Users/fenglongyang/Downloads/knowledge_representation/task3.in");
+        Core core = new Core("test/task3.in");
         core.run();;
     }
 
     @Test
     public void TestTask6() {
-        Core core = new Core("/Users/fenglongyang/Downloads/knowledge_representation/task6.in");
+        Core core = new Core("test/task6.in");
         core.run();
     }
 
     @Test
     public void TestTask7() {
-        Core core = new Core("/Users/fenglongyang/Downloads/knowledge_representation/task7.in");
+        Core core = new Core("test/task7.in");
         core.run();
     }
 }
